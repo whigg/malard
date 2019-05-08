454669ba68d4cda245ab509cf8afeb4c42a48c09
sjalford76 <s_j_alford@hotmail.com>
2019-05-08 16:36:56 +0100

    Update MalardHelpers.py
    Now create a column orientated DataFrame.

--- python/MalardHelpers.py
+++ python/MalardHelpers.py
@@ -3,11 +3,11 @@ import netCDF4
 
 def getDataFrameFromNetCDF(filePath):
     nc = netCDF4.Dataset(filePath)
-    data = []
+    data = {}
 
     for v in nc.variables:
         d = nc.variables[v]
-        srs = pandas.Series(d[:], name=v)
-        data.append(srs)
+        srs = pandas.Series(d[:])
+        data[v] = srs
 
     return pandas.DataFrame(data)
