4048272f62b27d548f58ceca55100cf4420074fe
sjalford76 <s_j_alford@hotmail.com>
2019-07-24 19:00:23 +0100

    Projections can be queried by regions and datasets. The proj4 codes are stored centrally in Configuration.Projections.

--- projection-api/src/main/scala/com/earthwave/projection/api/Messages.scala
+++ projection-api/src/main/scala/com/earthwave/projection/api/Messages.scala
@@ -8,3 +8,10 @@ object Projection
 {
   implicit val format : Format[Projection] = Json.format[Projection]
 }
+
+case class ProjectionMapping( parentDataSetName : String, region : String, shortName : String)
+
+object ProjectionMapping
+{
+  implicit  val format : Format[ProjectionMapping] = Json.format[ProjectionMapping]
+}
