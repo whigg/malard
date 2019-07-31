323d447dda59c09335172c9386c3afe28467db02
jon <s_j_alford@hotmail.com>
2019-07-31 10:28:00 +0100

    Merging Issue

--- point-impl/src/main/scala/com/earthwave/point/impl/NetCdfWriter.scala
+++ point-impl/src/main/scala/com/earthwave/point/impl/NetCdfWriter.scala
@@ -63,7 +63,7 @@ class NetCdfWriter( filename : String, val srcColumns : List[Column], val schema
               }
           })
 
-    rowcount = rowcount + mask.length - 1
+    rowcount = rowcount + mask.length
 }
   def write(variables : List[(Variable, ucar.ma2.Array)]) = {
     val origin = Array.ofDim[Int](1)
