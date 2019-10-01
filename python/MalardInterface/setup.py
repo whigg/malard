ba579896ab69cb993b72dd556d59e102105f886e
jon <s_j_alford@hotmail.com>
2019-10-01 13:40:07 +0100

    Setup script for pip installs.

---
+++ python/MalardInterface/setup.py
@@ -0,0 +1,41 @@
+import setuptools
+
+
+
+with open("README.md", "r") as fh:
+
+    long_description = fh.read()
+
+setuptools.setup(
+
+     name='MalardInterface',  
+
+     version='0.1',
+
+     scripts=['MalardInterface'] ,
+
+     author="Jonathan Alford",
+
+     author_email="jonathan@earthwave.co.uk",
+
+     description="MalardInterface is a web-client package for GEO Data Analytics",
+
+     long_description=long_description,
+
+   long_description_content_type="text/markdown",
+
+     url="https://github.com/earthwave/malard/tree/master/python/MalardInterface",
+
+     packages=setuptools.find_packages(),
+
+     classifiers=[
+
+         "Programming Language :: Python :: 3",
+
+         "License :: OSI Approved :: MIT License",
+
+         "Operating System :: OS Independent",
+
+     ],
+
+ )
