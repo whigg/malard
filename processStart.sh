91350aa1d5f2a2b408ca3e92926a62e6f43ea0da
jon <s_j_alford@hotmail.com>
2019-12-29 17:46:45 +0000

    Changed point publisher instances to 3.

--- processStart.sh
+++ processStart.sh
@@ -4,7 +4,11 @@ sbt -Dconfig.resource=swathProcessor1.conf "point-stream-impl/runMain com.earthw
 P5=$!
 sbt -Dconfig.resource=publishprocessor1.conf "point-stream-impl/runMain com.earthwave.pointstream.impl.PointWorkerProcess -instance=1 -worker=PublisherProcessor" &
 P9=$!
+sbt -Dconfig.resource=publishprocessor2.conf "point-stream-impl/runMain com.earthwave.pointstream.impl.PointWorkerProcess -instance=2 -worker=PublisherProcessor" &
+P10=$!
+sbt -Dconfig.resource=publishprocessor3.conf "point-stream-impl/runMain com.earthwave.pointstream.impl.PointWorkerProcess -instance=3 -worker=PublisherProcessor" &
+P11=$!
 
-wait $P1 $P5 $P9
+wait $P1 $P5 $P9 $P10 $P11
 
 
