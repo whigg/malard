ac872a8b6190d6b82c4d69b7c0bd7b1c7cb8f462
sjalford76 <s_j_alford@hotmail.com>
2019-12-29 20:22:20 +0000

    3 swath writers.

--- processStart.sh
+++ processStart.sh
@@ -2,6 +2,11 @@ sbt -Dconfig.resource=queryprocessor1.conf "point-stream-impl/runMain com.earthw
 P1=$!
 sbt -Dconfig.resource=swathProcessor1.conf "point-stream-impl/runMain com.earthwave.pointstream.impl.PointWorkerProcess -instance=1 -worker=SwathProcessor" &
 P5=$!
+sbt -Dconfig.resource=swathProcessor2.conf "point-stream-impl/runMain com.earthwave.pointstream.impl.PointWorkerProcess -instance=2 -worker=SwathProcessor" &
+P6=$!
+sbt -Dconfig.resource=swathProcessor3.conf "point-stream-impl/runMain com.earthwave.pointstream.impl.PointWorkerProcess -instance=3 -worker=SwathProcessor" &
+P7=$!
+
 sbt -Dconfig.resource=publishprocessor1.conf "point-stream-impl/runMain com.earthwave.pointstream.impl.PointWorkerProcess -instance=1 -worker=PublisherProcessor" &
 P9=$!
 sbt -Dconfig.resource=publishprocessor2.conf "point-stream-impl/runMain com.earthwave.pointstream.impl.PointWorkerProcess -instance=2 -worker=PublisherProcessor" &
@@ -9,6 +14,6 @@ P10=$!
 sbt -Dconfig.resource=publishprocessor3.conf "point-stream-impl/runMain com.earthwave.pointstream.impl.PointWorkerProcess -instance=3 -worker=PublisherProcessor" &
 P11=$!
 
-wait $P1 $P5 $P9 $P10 $P11
+wait $P1 $P5 $P6 $P7 $P9 $P10 $P11
 
 
