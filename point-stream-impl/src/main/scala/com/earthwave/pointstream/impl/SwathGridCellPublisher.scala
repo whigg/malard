00aac0961f3cade6a9551de9df2c033f41bc1aba
sjalford76 <s_j_alford@hotmail.com>
2020-01-20 12:55:58 +0000

    Performance improvement to the shapefile masking.

--- point-stream-impl/src/main/scala/com/earthwave/pointstream/impl/SwathGridCellPublisher.scala
+++ point-stream-impl/src/main/scala/com/earthwave/pointstream/impl/SwathGridCellPublisher.scala
@@ -135,6 +135,7 @@ class SwathGridCellPublisher( val idx : Int ) extends Actor {
                 indices.append(i)
               }
               mask.+=((gridCell, indices))
+              regionMask.+=((gridCell, regionMask))
             }
           }
 
