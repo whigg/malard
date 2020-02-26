95b6653a642c6f24b7bfa4295f122f4e898ff419
jon <jonathan@earthwave.co.uk>
2020-02-26 14:49:49 +0000

    set the months and years properly.

--- python/DataLoading/parallel_run.py
+++ python/DataLoading/parallel_run.py
@@ -5,7 +5,9 @@ import sys
 def main(script, processes):
     months = range(1,12+1)
     years = [2011,2012,2019,2020]
-
+    
+    monthsandyears = sum([ [ (m,y) for m in months ]  for y in years ],[])
+    
     fmt_cmd =  '"python {script}.py {month} {year} >> log/{script}_{year}_{month}.txt"'
 
     cmds = [  fmt_cmd.format(script=script, month=m,year=y) for m,y in monthsandyears ]
