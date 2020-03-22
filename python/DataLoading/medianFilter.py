6d7d3272ce1844156956081edd87b117d5fed50e
jon <jonathan@earthwave.co.uk>
2020-03-22 20:36:44 +0000

    added MALARD_PROD

--- python/DataLoading/medianFilter.py
+++ python/DataLoading/medianFilter.py
@@ -9,7 +9,10 @@ Created on Wed Mar 11 16:22:16 2020
 
 import math
 import numpy as np
+import matplotlib.pyplot as plt
 import os
+
+import Timeseries as t
 
 from osgeo import gdal
 
@@ -39,6 +42,8 @@ def nanmedfilt2(a,m,n):
     b = np.empty(a.shape)
     b.fill(np.nan)
     
+    coverage = np.zeros(a.shape)
+    
     NRows, NCols = a.shape
     
     mhalf = math.floor(m/2)
@@ -54,30 +59,38 @@ def nanmedfilt2(a,m,n):
             
             if not np.all(np.isnan(tmp)):
                 b[i,j] = np.nanmedian(tmp)
+                #check the corners all have data
+                if( not np.isnan(tmp[0,0]) and not np.isnan(tmp[0,mhalf]) and not np.isnan(tmp[mhalf,mhalf]) and not np.isnan(tmp[mhalf,0])):
+                    coverage[i,j] = 1
             else:
                 b[i,j] = np.nan
-    return b
+    return (b,coverage)
 
-def medClean(mat, m, n , iterations):
+def medClean(mat, demArray, m, n , iterations):
     
     rows, cols = mat.shape
+    
     
     for i in range(rows):
         for j in range(cols):
             mat[i,j] = np.nan if mat[i,j] == -32768 else mat[i,j]
     
-    outMatrices = []
+    outMat = np.empty(mat.shape)
+    outMat.fill(np.nan)
+    
+    adjustment = np.empty(mat.shape)
+    adjustment.fill( np.nan )
+    
     for it in range(iterations):
         
-        matF = nanmedfilt2( mat, m, n )
-        
+        matF, coverage = nanmedfilt2( mat, m, n )
         
         print( np.nanmax(mat) )
         print( np.nanmin(mat) )
         
         print( np.nanmax(matF) )
         print( np.nanmin(matF) )
-        diff = mat - matF
+        diff = matF - mat
         
         mad = np.nanstd( diff )
         print(mad)
@@ -85,51 +98,75 @@ def medClean(mat, m, n , iterations):
         absDiff = np.abs( diff )
         print(np.nanmedian(absDiff))
         
-        outMat = np.empty(mat.shape)
-        outMat.fill(np.nan)
+        
         for i in range(rows):
             for j in range(cols):
-                outMat[i,j] = mat[i,j] if absDiff[i,j] < 3*mad and not np.isnan(absDiff[i,j]) else np.nan
+                if absDiff[i,j] < 3*mad and not np.isnan(absDiff[i,j]): 
+                    outMat[i,j] = mat[i,j]
+                elif coverage[i,j] == 1:
+                    adjustment[i,j] = diff[i,j]
+                    outMat[i,j] = matF[i,j]
+                else:
+                    outMat[i,j] = np.nan
         
-        outMatrices.append( outMat )
-        print(type(outMat))
         mat = outMat
     
     mask = np.zeros(outMat.shape)
     
     for i in range(rows):
         for j in range(cols):
-            outMat[i,j] = -32768 if np.isnan(outMat[i,j])  else outMat[i,j]
-            mask[i,j] = 1 if outMat[i,j] > -32768 else 0
+            mask[i,j] = 1 if not np.isnan(outMat[i,j]) or not np.isnan(adjustment[i,j]) else 0
+            
+            if mask[i,j] == 1:
+                if not np.isnan(adjustment[i,j]):
+                    demArray[i,j] = demArray[i,j] + adjustment[i,j] 
+            else:
+                demArray[i,j] = -32768
     
-    return (outMat, mask)
+    return (mask, adjustment)
 
-def applyMedianFilter( diffFilePath, numIterations ):
+def applyMedianFilter( demFilePath, diffFilePath, numIterations ):
     fullPath = diffFilePath
 
     array = getArray( fullPath )        
+    demArray = getArray( demFilePath )
     
-    outMat, mask = medClean( array, 5,5, numIterations  )
+    mask, adjustment = medClean( array, demArray, 5,5, numIterations  )
 
     driver = gdal.GetDriverByName('GTiff')
-    diffMaskFileName = fullPath.replace("_diff","_diff_medianmask_{}".format(numIterations))
-    result = driver.CreateCopy(diffMaskFileName, gdal.Open(fullPath))
-    result.GetRasterBand(1).WriteArray(outMat)
-    result = None
     
     maskFileName = fullPath.replace("_diff","_medianmask_{}".format(numIterations))
     result = driver.CreateCopy(maskFileName, gdal.Open(fullPath))
     result.GetRasterBand(1).WriteArray(mask)
+    result = None   
+    
+    demFileName = fullPath.replace(".tif","_masked.tif")
+    result = driver.CreateCopy(demFileName, gdal.Open(fullPath))
+    result.GetRasterBand(1).WriteArray(demArray)
     result = None
     
-    return maskFileName
+    return demFileName
 
 if __name__ == "__main__":
     
-    filePath =  "/data/puma/scratch/cryotempo/processeddata/greenland_nw_adjust/GrIS_NWFW115_pocaDemDiff100_Unc_7_MinP_1_MaxPix_8_DemDiffMad_6_Resolution_2000"
+    
+    arcticDem = '/data/puma/scratch/cryotempo/underlying_dems/greenland_arctic_dem/combined/GrIS_dem_2km.tif'
+    filePath =  "/data/eagle/team/shared/Work/CryoTEMPO/PM5/DemDiffMadRatio/GrIS_NWFW115_pocaDemDiff100_Unc_7_MinP_1_MaxPix_8_DemDiffMad_6_Resolution_2000"
     diffFile = "greenland_2011_2_diff.tif"
     dem = "greenland_2011_2.tif"
     numIterations = 4
     
-    applyMedianFilter(filePath, dem, diffFile, numIterations)
-
+    demFilePath = os.path.join( filePath, dem )
+    diffFilePath = os.path.join(filePath, diffFile )
+    
+    demFileName = applyMedianFilter(demFilePath, diffFilePath, numIterations)
+    ## add the code to do the diff
+    fig, ax = plt.subplots(1,2, figsize=(6,12))
+    t.nicePlot(demFileName,fig,ax[0],"Feb2011 MaskedDem","Elevation (m)",nodata=-32768)
+    t.nicePlot(demFilePath,fig,ax[1],"Feb2011 Dem","Elevation (m)",nodata=-32768)
+    
+    plot = demFileName.replace(".tif",".png")
+    print("About to save figure {}".format(plot) )
+    fig.savefig( plot )
+    
+     
